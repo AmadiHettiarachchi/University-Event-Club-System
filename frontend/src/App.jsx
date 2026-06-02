31c6d9a3aa968d7a8a0d06fa6314c292b92de1f9
IT23219434 <IT23219434@my.sliit.lk>
2026-06-02 22:05:28 +0530

    updated system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,11 +7,12 @@ import StudentDashboard from "./pages/StudentDashboard";
 import ClubLeaderDashboard from "./pages/ClubLeaderDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import CreateEvent from "./pages/CreateEvent";
-import EventPost from "./pages/EventPost";
 import EditEvent from "./pages/EditEvent";
+import EventPost from "./pages/EventPost";
 import MarkAttendance from "./pages/MarkAttendance";
 import SubmitFeedback from "./pages/SubmitFeedback";
 import CreateAnnouncement from "./pages/CreateAnnouncement";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -18,17 +20,87 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/student-dashboard" element={<StudentDashboard />} />
-      <Route path="/club-leader-dashboard" element={<ClubLeaderDashboard />} />
-      <Route path="/admin-dashboard" element={<AdminDashboard />} />
-      <Route path="/create-event" element={<CreateEvent />} />
-      <Route path="/event-post" element={<EventPost />} />
-      <Route path="/edit-event/:id" element={<EditEvent />} />
-      <Route path="/mark-attendance" element={<MarkAttendance />} />
-      <Route path="/submit-feedback" element={<SubmitFeedback />} />
-      <Route path="/create-announcement" element={<CreateAnnouncement />} />
 
+      <Route
+        path="/student-dashboard"
+        element={
+          <ProtectedRoute allowedRoles={["student"]}>
+            <StudentDashboard />
+          </ProtectedRoute>
+        }
+      />
 
+      <Route
+        path="/club-leader-dashboard"
+        element={
+          <ProtectedRoute allowedRoles={["clubLeader"]}>
+            <ClubLeaderDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin-dashboard"
+        element={
+          <ProtectedRoute allowedRoles={["admin"]}>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/create-event"
+        element={
+          <ProtectedRoute allowedRoles={["clubLeader"]}>
+            <CreateEvent />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/edit-event/:id"
+        element={
+          <ProtectedRoute allowedRoles={["clubLeader"]}>
+            <EditEvent />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/event-post"
+        element={
+          <ProtectedRoute allowedRoles={["clubLeader"]}>
+            <EventPost />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/mark-attendance"
+        element={
+          <ProtectedRoute allowedRoles={["clubLeader"]}>
+            <MarkAttendance />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/create-announcement"
+        element={
+          <ProtectedRoute allowedRoles={["clubLeader"]}>
+            <CreateAnnouncement />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/submit-feedback"
+        element={
+          <ProtectedRoute allowedRoles={["student"]}>
+            <SubmitFeedback />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
